3ab8a1d5f907e3ca8155928d0fdf906648c08a17
柯宇 潘 <1414463123@qq.com>
2018-12-25 13:31:49 +0800

    WIP

--- Hacker.py
+++ Hacker.py
@@ -10,15 +10,13 @@ class Hacker(pygame.sprite.Sprite):
     def __init__(self, site, scale, timer, lin_reg):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([3, 3])
-        self.color = (100, 0, 0)
-        self.image.fill(self.color)
         self.site = site
         self.lin_reg = lin_reg
         self.timer = timer
         self.HP = 100
         self.target = None
         self.pos = [(site.pos[0] + random.random() * 20 - 10) * scale, (site.pos[1] + random.random() * 20 - 10) * scale]
-        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
         self.state = "OFF"
         self.duration = [-1, -1]
         self.start_time = []
